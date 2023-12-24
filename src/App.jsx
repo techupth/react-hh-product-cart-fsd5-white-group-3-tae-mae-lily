982bc499681c5014c7a83bce7331aa63ff578bab
Okubo Hazuki <okubohazuki@Okubos-MacBook-Air.local>
2023-12-25 04:23:27 +0900

    practiced hh

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,85 @@
 import "./App.css";
+import "./data/products";
+import React, { useState } from "react";
+import products from "./data/products";
 
-function App() {
+const App = () => {
+  // State Hooks
+  const [cart, setCart] = useState([]);
+  const [totalPrice, setTotalPrice] = useState("0");
+  
+  const addToCart = (products) => {
+    //Updating the Cart Array
+    const updatedCart = [...cart, { ...products, quantity: 1 }];
+    setCart(updatedCart);
+    calculatePrice(updatedCart);
+  };
+
+  const removeFromCart = (products) => {
+    const updatedCart = cart.filter((item) => item.id !== products.id);
+    setCart(updatedCart);
+    calculatePrice(updatedCart);
+  };
+
+  const updateQuantity = (product, action) => {
+    const updatedCart = cart.map((item) => {
+      if (item.id === product.id) {
+        item.quantity += action === "add" ? 1 : -1;
+      }
+      return item;
+    });
+    setCart(updatedCart);
+    calculatePrice(updatedCart);
+  };
+
+  const calculatePrice = (cart) => {
+    const total = cart.reduce((acc, item) => acc + item.price * item.quantity, 0);
+    setTotalPrice(total);
+  };
+ 
   return (
     <div className="App">
       <section className="product-container">
         <h1 className="product-heading">Products</h1>
         <div className="product-list">
-          <div className="product">
-            <img
-              src="http://dummyimage.com/350x350.png/dddddd/000000"
-              alt="sample name"
-            />
-            <h2>Sample name</h2>
-            <p>Sample desc</p>
-            <button>Add to cart</button>
-          </div>
+
+          {/*Rendering Products*/}
+          {products.map((product) => (
+             <div className="product" key={product.id}>
+             <img src={product.image} alt={product.name}/>
+             <h2>{product.name}</h2>
+             <h2>{product.price} Baht</h2>
+             <p>{product.description}</p>
+             <button onClick={() => addToCart(product)}>Add to cart</button>
+           </div>
+           ))}
         </div>
       </section>
       <hr />
 
       <section className="cart">
-        <h1 className="cart-heading">Cart (Total Price is x Baht)</h1>
+        <h1 className="cart-heading">Cart (Total Price is {totalPrice} Baht)</h1>
         <div className="cart-item-list">
-          <div className="cart-item">
-            <h1>Item name: Fond - Neutral</h1>
-            <h2>Price: 160 Baht</h2>
-            <h2>Quantity: 2</h2>
-            <button className="delete-button">x</button>
+
+        {/*Rendering cartItem*/}  
+        {cart.map((cartItem) => (
+          <div className="cart-item" key={String(cartItem.id)}>
+            <h1>Item name: {cartItem.name}</h1>
+            <h2>Price: {cartItem.price} Baht</h2>
+            <h2>Quantity: {cartItem.quantity}</h2>
+            <button className="delete-button" onClick={() => removeFromCart(cartItem)}>x</button>
+
             <div className="quantity-actions">
-              <button className="add-quantity">+</button>
-              <button className="subtract-quantity">-</button>
+              <button className="add-quantity" onClick={() => updateQuantity(cartItem, 'add')}>+</button>
+              <button className="subtract-quantity" onClick={() => updateQuantity(cartItem, 'subtract')}>-</button>
             </div>
+
           </div>
+        ))};
         </div>
       </section>
-    </div>
-  );
+    </div>  
+  )
 }
 
 export default App;
